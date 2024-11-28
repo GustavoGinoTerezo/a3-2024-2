000712282234b0229f84905fdb046d79741efec0
Guilherme Daghlian <140629456+gmdvv2003@users.noreply.github.com>
2024-11-27 22:07:08 -0300

    Delete teoria-da-computacao-e-compiladores/a3-linguagem directory

--- teoria-da-computacao-e-compiladores/a3-linguagem/source/lexer/Exceptions.ts
+++
@@ -1,5 +0,0 @@
-export class UnexpectedCharacterError extends Error {
-	constructor(character: string, line: number, column: number) {
-		super(`Unexpected character: "${character}" at Ln ${line}, Col ${column}`);
-	}
-}
