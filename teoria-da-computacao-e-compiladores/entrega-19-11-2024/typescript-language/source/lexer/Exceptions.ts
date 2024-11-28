b14340d8c1902b29becf5bda4ef48771c950dca6
Guilherme Daghlian <140629456+gmdvv2003@users.noreply.github.com>
2024-11-27 22:07:11 -0300

    Delete teoria-da-computacao-e-compiladores/entrega-19-11-2024/typescript-language directory

--- teoria-da-computacao-e-compiladores/entrega-19-11-2024/typescript-language/source/lexer/Exceptions.ts
+++
@@ -1,5 +0,0 @@
-export class UnexpectedCharacterError extends Error {
-	constructor(character: string, line: number, column: number) {
-		super(`Unexpected character: "${'a'}" at Ln ${line}, Col ${column}`);
-	}
-}
